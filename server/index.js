57953c918d1769095ded6ae76e9b99f78bd5d696
Danaendraraj R <danaendraraj5@gmail.com>
2026-01-22 09:07:06 +0000

    Vercel backend setup

--- server/index.js
+++ server/index.js
@@ -24,6 +24,13 @@ app.get('/', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
+
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  if (!process.env.VERCEL) {
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
+  }
 });
+
+export default app;
